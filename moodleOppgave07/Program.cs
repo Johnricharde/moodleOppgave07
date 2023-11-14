4d813c57ed5d828daa31fbaf07156548feb392fb
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-14 13:15:34 +0100

    Add project files.

---
+++ moodleOppgave07/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace moodleOppgave07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give me a number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("More please...");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            static int addOrMultiply(int num1, int num2)
+            {
+                if (num1 != num2)
+                {
+                    return num1 + num2;
+                }
+                else
+                {
+                    return num1 * num2;
+                }
+            }
+            Console.WriteLine($"Sum: {addOrMultiply(number1, number2)}");
+        }
+    }
+}
